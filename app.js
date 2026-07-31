8f53505b06aa6237118daeeb77ee0fd5cae02979
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-07-31 12:26:08 -0400

    Setup: Tailwind CSS and Daisy UI

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.use(expressSession({
     cookie: {
